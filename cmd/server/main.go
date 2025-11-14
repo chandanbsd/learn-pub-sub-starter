455e31341cd2251b2d048d3c64c9cf5995c31864
Chandan Bangalore Shankar Denanath <49138440+chandanbsd@users.noreply.github.com>
2025-11-13 23:38:28 -0500

    Updates

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
+
+	connString := "amqp://guest:guest@localhost:5672/"
+
+	con, err := amqp.Dial(connString)
+	if err != nil {
+		panic("Failed to setup the connection")
+	}
+
+	defer con.Close()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	
+	for _ = range signalChan {
+		fmt.Printf("Interrupt signal received. Shutting down gracefully.")
+		os.Exit(0)
+	}
+
+	fmt.Print("rabbitmq connection successfully established")
+
 	fmt.Println("Starting Peril server...")
 }
